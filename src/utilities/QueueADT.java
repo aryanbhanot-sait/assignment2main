88db91c1a28b223e7585dffd6f6c1fa8ea5d039a
Amrit Reddy <amrit_reddy@me.com>
2025-07-12 15:23:16 -0600

    Update QueueADT.java, StackADT.java
    Update QueueADT.java; use custom Iterator (utilities.Iterator), add contains(), toArray(), isFull and equals() methods. Add notes.
    Update StackADT.java; use custom Iterator (utilities.Iterator), add contains(), toArray(), search() methods. Add notes.

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -1,14 +1,89 @@
 package utilities;
-
 import exceptions.EmptyQueueException;
 
-public interface QueueADT<E> {
+/**
+ * The QueueADT interface defines the basic operations for a queue data structure.
+ * @param <E> the type of elements in the queue
+ */
 
+public interface QueueADT<E> 
+{
+    /**
+     * Adds an element to the end of the queue.
+     * @param element the element to be added to the queue
+     * @throws NullPointerException if the element is null
+     */
     void enqueue(E element) throws NullPointerException;
+
+    /**
+     * Removes and returns the front element from the queue.
+     * @return the element that was removed from the front of the queue
+     * @throws EmptyQueueException if the queue is empty
+     */
     E dequeue() throws EmptyQueueException;
+
+    /**
+     * Returns the front element of the queue without removing it.
+     * @return the front element of the queue
+     * @throws EmptyQueueException if the queue is empty
+     */
     E peek() throws EmptyQueueException;
+
+    /**
+     * Checks if the queue is empty.
+     * @return true if the queue is empty, false otherwise
+     */
     boolean isEmpty();
+
+    /**
+     * Returns the number of elements in the queue.
+     * @return the size of the queue
+     */
     int size();
+
+    /**
+     * Clears the queue, removing all elements.
+     */
     void clear();
-    java.util.Iterator<E> iterator();
+
+    /**
+     * Returns an iterator over the elements in the queue.
+     * @return an iterator for the queue
+     */
+    utilities.Iterator<E> iterator();
+
+    /**
+     * Checks if the queue contains a specific element.
+     * @param element the element to check for
+     * @return true if the queue contains the element, false otherwise
+     * @throws NullPointerException if the element is null
+     */
+    boolean contains(E element) throws NullPointerException;
+
+    /**
+     * Returns an array containing all elements in the queue.
+     * @return an array of elements in the queue
+     */
+    Object[] toArray();
+
+    /**
+     * Returns an array containing all elements in the queue, with the specified type.
+     * @param arr the array to fill with elements from the queue
+     * @return an array containing the elements of the queue
+     * @throws NullPointerException if the provided array is null
+     */
+    E[] toArray(E[] arr) throws NullPointerException;
+
+    /**
+     * Checks if the queue is full.
+     * @return true if the queue is full, false otherwise
+     */
+    boolean isFull();
+
+    /**
+     * Compares this queue with another queue for equality.
+     * @param otherQueue the queue to compare with
+     * @return true if both queues are equal, false otherwise
+     */
+    boolean equals(QueueADT<E> otherQueue);
 }
--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -1,12 +1,83 @@
 package utilities;
 
-public interface StackADT<E> {
+/**
+ * The StackADT interface defines the basic operations for a stack data structure.
+ * @param <E> the type of elements in the stack
+ */
 
+public interface StackADT<E> 
+{
+    /**
+     * Pushes an element onto the top of the stack.
+     * @param element the element to be pushed onto the stack
+     * @throws NullPointerException if the element is null
+     */
     void push(E element) throws NullPointerException;
+
+    /**
+     * Removes and returns the top element from the stack.
+     * @return the element that was popped from the stack
+     * @throws java.util.EmptyStackException if the stack is empty
+     */
     E pop() throws java.util.EmptyStackException;
+
+    /**
+     * Returns the top element of the stack without removing it.
+     * @return the top element of the stack
+     * @throws java.util.EmptyStackException if the stack is empty
+     */
     E peek() throws java.util.EmptyStackException;
+
+    /**
+     * Checks if the stack is empty.
+     * @return true if the stack is empty, false otherwise
+     */
     boolean isEmpty();
+
+    /**
+     * Returns the number of elements in the stack.
+     * @return the size of the stack
+     */
     int size();
+
+    /**
+     * Clears the stack, removing all elements.
+     */
     void clear();
-    java.util.Iterator<E> iterator();
+
+    /**
+     * Returns an iterator over the elements in the stack.
+     * @return an iterator for the stack
+     */
+    utilities.Iterator<E> iterator();
+
+    /**
+     * Checks if the stack contains a specific element.
+     * @param element the element to check for
+     * @return true if the stack contains the element, false otherwise
+     * @throws NullPointerException if the element is null
+     */
+    boolean contains(E element) throws NullPointerException;
+
+    /**
+     * Returns an array containing all elements in the stack.
+     * @return an array of elements in the stack
+     */
+    Object[] toArray();
+
+    /**
+     * Returns an array containing all elements in the stack, with the specified type.
+     * @param arr the array to store the elements
+     * @return an array containing the elements of the stack
+     * @throws NullPointerException if the provided array is null
+     */
+    E[] toArray(E[] arr) throws NullPointerException;
+
+    /**
+     * Searches for an element in the stack and returns its position.
+     * @param element the element to search for
+     * @return the position of the element in the stack, or -1 if not found
+     * @throws NullPointerException if the element is null
+     */
+    int search(E element) throws NullPointerException;
 }
